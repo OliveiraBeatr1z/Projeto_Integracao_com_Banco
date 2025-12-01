592730bca1833eca1b36768587568aadf66fc5d6
OliveiraBeatr1z <luamorningstar3@gmail.com>
2025-12-01 19:02:13 -0300

    funciona na minha maquina

--- src/main/java/br/com/bank/BytebankApplication.java
+++ src/main/java/br/com/bank/BytebankApplication.java
@@ -2,15 +2,18 @@ package br.com.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BytebankApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BytebankApplication.class, args);
-        System.out.println("\n=== ByteBank API está rodando! ===");
-        System.out.println("Acesse: http://localhost:8080/api/contas");
-        System.out.println("=====================================\n");
+        ConfigurableApplicationContext ctx = SpringApplication.run(BytebankApplication.class, args);
+        Environment env = ctx.getEnvironment();
+        String port = env.getProperty("local.server.port");
+        System.out.println("=== ByteBank API está rodando! ===");
+        System.out.println("Acesse: http://localhost:" + port + "/api/contas");
+        System.out.println("=====================================");
     }
 }
-
